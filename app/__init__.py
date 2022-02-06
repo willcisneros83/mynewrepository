4c957856b1a08592ae151de91a3789261ec243b1
Will Cisneros <will.cisneros@yahoo.com>
2022-02-06 13:52:37 -0600

    Added about me

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,21 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "<h1>Hello, world</h1>"
+
+@app.route("/products")
+def products():
+    product_list = ["Apples", "Oranges", "Bananas"]
+    bullet_list =  "".join(
+        "<li>%s</li>" % product for product in product_list
+        )
+    return "<ul>%s</ul>" % bullet_list 
+
+@app.route("/about", methods=["GET"])
+def aboutme():
+    me = {
+        "first_name":"Will",
+        "last_name": "Cisneros",
+        "hobbies": "DIY stuff",
+        "active": True 
+    }
+    return me
